6095e215a039ce881da8de0e89f67d95d2bae000
Tushar Sharma <tusharsharma2822@gmail.com>
2025-04-26 15:47:50 +0530

    update user.routes.js to handle user-related-routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as userController from "../controllers/user.controller.js"
 import { body } from "express-validator";
+import * as authMiddleware from "../middleware/auth.middleware.js";
 
 const router = Router();
 
@@ -10,6 +11,23 @@ router.post("/register", [
     body("password").isLength({ min: 3 }).withMessage("Password must be atleast 3 characters long")
 ],
     userController.createUserController
+)
+
+router.post("/login", [
+    body("email").isEmail().withMessage("Email must be a valid address"),
+    body("password").isLength({ min: 3 }).withMessage("Password must be atleast 3 characters long")
+],
+    userController.loginUserController
+)
+
+router.get("/profile",
+    authMiddleware.authUser,
+    userController.getUserProfileController
+)
+
+router.get("/logout", 
+    authMiddleware.authUser,
+    userController.logoutUserController
 )
 
 export default router;
