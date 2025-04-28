f7ff802551f836e9e9b9d15951d1f1409aaed533
Tushar Sharma <tusharsharma2822@gmail.com>
2025-04-28 12:23:24 +0530

    update auth.middleware.js to handle authentication and authorization of users and captains

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,6 +1,7 @@
 import userModel from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import captainModel from "../models/captain.model.js";
 
 export const authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization.split(' ')[1];
@@ -20,5 +21,27 @@ export const authUser = async (req, res, next) => {
         
     } catch (err) {
         return res.status(401).json({ message: "Unauthorized Access" });
+    }
+}
+
+export const authCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+    if(!token){
+        return res.status(401).json({ message: "Unauthorized Access"});
+    }
+
+    try {
+        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const captain = await captainModel.findById(decoded._id);
+
+        req.captain = captain;
+
+        return next();
+
+    } catch (err) {
+        return res.status(401).json({ message: "Unauthorized Access"});
     }
 }
